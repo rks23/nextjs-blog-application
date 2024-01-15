1c6ac68441d764f073e5dd53824454d3cc0305d0
rahul <dev.rpcs@gmai.com>
2024-01-15 14:37:49 +0545

    next auth setup done

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,38 +1,61 @@
+import bcrypt from "bcrypt";
 import NextAuth, { AuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
-import CredentialsProvider from "next-auth/providers/credentials"
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectToDb } from "@/lib/helpers";
+import primsa from "@/prisma";
 
-const authOptions: AuthOptions = {
+export const authOptions: AuthOptions = {
   providers: [
     GithubProvider({
       clientId: "",
-      clientSecret: ""
+      clientSecret: "",
     }),
     GoogleProvider({
       clientId: "",
-      clientSecret: ""
+      clientSecret: "",
     }),
     CredentialsProvider({
       name: "credentials",
       credentials: {
         email: { type: "text" },
-        password: { type: "password" }
+        password: { type: "password" },
       },
-      authorize({ credentials }, req) {
+      async authorize(credentials, req) {
         if (!credentials || !credentials.email || !credentials.password) {
           return null;
         }
         try {
+          await connectToDb();
+          const user = await primsa.user.findFirst({
+            where: { email: credentials.email },
+          });
 
+          if (!user) {
+            return null;
+          }
+          if (!user.password) {
+            return null;
+          }
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+          if (!isPasswordCorrect) {
+            return null;
+          }
+          return { ...user, id: user.id };
         } catch (error) {
-
+          return null;
+        } finally {
+          await primsa.$disconnect();
         }
-      }
-    })
+      },
+    }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-}
+};
 
 const handler = NextAuth(authOptions);
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <main>Hello World!</main>;
+// import { getServerSession } from "next-auth";
+// import { authOptions } from "./api/auth/[...nextauth]/route";
+"use client";
+
+import { useSession } from "next-auth/react";
+
+export default /*async*/ function Home() {
+  // const data = await getServerSession(authOptions);
+  const { data, status } = useSession();
+  return (
+    <main>
+      <div>Hello World!</div>
+      <div>{JSON.stringify(data)}</div>
+      <div>{JSON.stringify(status)}</div>
+    </main>
+  );
 }
